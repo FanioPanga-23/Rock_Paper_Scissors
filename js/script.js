75c8cd07419fa4d6377118742546b755289a3857
f-99 <faniomears@gmail.com>
2026-06-03 21:47:39 +0100

    Refatored: rock, paper and scissors

--- js/script.js
+++ js/script.js
@@ -1,82 +1,90 @@
+let     humanScore = 0;
+let     computerScore = 0;
 let     rock = document.querySelector( ".rock" );
 let     paper = document.querySelector( ".paper" );
 let     scissors = document.querySelector( ".scissors" );
 let     score = document.querySelector( ".score" );
+let     inputCPU = document.getElementById( "cpu" );
+let     inputHuman = document.getElementById( "human" );
 
-rock.addEventListener( "click", ()=> {console.log("Chamou!!")});
+rock.addEventListener( "click", () => {
+    checkWinner( "rock", computerChoice() );
+    endGame();
+} );
 
+paper.addEventListener( "click", () => {
+    checkWinner( "paper", computerChoice() );
+    endGame();
+} );
 
-let     humanScore;
-let     computerScore;
-const   rock1 = "rock";
-const   paper1 = "paper";
-const   scissors1 = "scissors";
-
-humanScore = 0;
-computerScore = 0;
+scissors.addEventListener( "click", () => {
+    checkWinner( "scissors", computerChoice() );
+    endGame();
+} );
 
 function    computerChoice()
 {
     let     choice;
     if (Math.round(1 + Math.random() * 2) === 1)
-        return (rock);
+        return ( "rock" );
     else if (Math.round(1 + Math.random() * 2) === 2)
-        return (paper);
+        return ( "paper" );
     else
-        return (scissors);
+        return ( "scissors" );
 }
 
 function checkWinner(player1, player2)
 {
     let     winner;
 
-    if (player1 === player2)
-        return (alert("Draw"));
-
     winner = "unknown";
-    if (player1 === rock && player2 === paper)
+    if (player1 === "rock" && player2 === "paper")
         winner = "Computer";
-    else if (player2 === rock && player1 === paper)
+    else if (player2 === "rock" && player1 === "paper")
         winner = "You";
-    else if (player1 === rock && player2 === scissors)
+    else if (player1 === "rock" && player2 === "scissors")
         winner = "You";
-    else if (player2 === rock && player1 === scissors)
+    else if (player2 === "rock" && player1 === "scissors")
         winner = "Computer";
-    else if (player2 == paper && player1 == scissors)
+    else if (player2 == "paper" && player1 == "scissors")
         winner = "You";
-    else if (player1 == paper && player2 == scissors)
+    else if (player1 == "paper" && player2 == "scissors")
         winner = "Computer";
 
-    return (winner);
+    chooseWinner( winner );
 }
 
 function    chooseWinner( winner )
 {
     if (winner === "You")
-    {
         humanScore++;
-        alert(`${winner} win`);
-    }
     else if (winner === "Computer")
-    {
         computerScore++;
-        alert(`${winner} wins`);
-    }
-    alert(`Score:\nYOU ${humanScore} - ${computerScore} CPU`);
+    updateScore();
 }
 
-function    playRound(player1, player2 )
+function    updateScore()
 {
-    return (chooseWinner(checkWinner(player1, player2)));
+    inputHuman.value = `${humanScore}`;
+    inputCPU.value = `${computerScore}`;
 }
 
-function playGame()
+function    resetScore()
 {
-    for (let i = 1; i <= 5; i++)
+    humanScore = 0;
+    computerScore = 0;
+    inputCPU.value = 0;
+    inputHuman.value = 0;
+}
+
+function    endGame()
+{
+    if ( humanScore > 4 || computerScore > 4 )
     {
-        alert(`Round ${i}`);
-        playRound(humanChoice(), computerChoice());
+        if ( humanScore > 4 )
+            alert("Game Over.\n\t\tYou win!");
+        else
+            alert( "Game Over.\n\t\tCPU win!");
+        resetScore();
     }
 }
-
-playGame();
